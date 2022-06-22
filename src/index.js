3fe60d005c617c3f32ccd49004d134a18998e06c
Sarah Mukuti <cesarwrites24@gmail.com>
2022-06-22 12:27:51 +0300

    add function to randomly generate quote

--- src/index.js
+++ src/index.js
@@ -19,3 +19,38 @@ fetch ('https://type.fit/api/quotes')
 })
 }
 
+function generateRandomQuote(){
+    
+}
+
+//const quotesPromise = fetchQuotes();
+const quoteEl = document.getElementById('quote');
+const quoteAuthorEl = document.getElementById('author');
+
+document.getElementById('new-quote').addEventListener('click', function (e){
+    fetch('https://type.fit/api/quotes')
+    .then(response=>response.json())
+    .then (quotes => {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    const quoteText = quotes[randomQuoteIndex].text;
+    let author = quotes[randomQuoteIndex].author;
+
+    if (!author) {
+        author = 'Unknown';
+    }
+
+    if (quoteText.length > 120) {
+        quoteEl.classList.add('long-quote');
+    } else {
+        quoteEl.classList.remove('long-quote');
+    }
+    quoteEl.textContent = quoteText;
+    quoteAuthorEl.textContent = author;
+});
+
+});
+
+
+
+
+
